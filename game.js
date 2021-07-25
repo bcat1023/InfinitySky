4a5354d44bfcffa24776f10f2b9ebe18ce069ad1
bcat1023 <bthecat984@gmail.com>
2021-07-24 21:29:09 -0700

    test 2

--- game.js
+++ game.js
@@ -1,3 +1,21 @@
-var load = //accomplishedlastingdisc.bcat1023.repl.co/game/logon.js
+function getPostData(url, toSend, async, method) {
 
-load
+  // Create new request
+  var xhr = new XMLHttpRequest()
+
+  // Set parameters
+  xhr.open('POST', url, async)
+
+  // Add event listener
+  xhr.onreadystatechange = function () {
+
+      // Check if finished
+      if (xhr.readyState == 4 && xhr.status == 200) {
+
+          // Do something with data
+          method(xhr.responseText);
+          console.log(xmr.responseText);
+
+      }
+  }
+}
